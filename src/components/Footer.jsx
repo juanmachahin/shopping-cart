378d8d37e13e33add2816eb262bb914f645f5b52
juanmachahin <juanma.chahin@gmail.com>
2023-09-07 18:55:05 -0400

    cuarto commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,6 @@ export function Footer () {
 
   return (
     <footer className='footer'>
-      <h4>Prueba técnica de React ⚛️ － <span>@midudev</span></h4>
       <h5>Shopping Cart con useContext & useReducer</h5>
     </footer>
   )
